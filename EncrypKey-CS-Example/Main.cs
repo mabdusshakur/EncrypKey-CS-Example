9d6d707652b7105e98bf104fa9c496f9cbf9fca7
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-02-16 12:16:51 +0600

    url removed

--- EncrypKey-CS-Example/Main.cs
+++ EncrypKey-CS-Example/Main.cs
@@ -14,13 +14,12 @@ namespace EncrypKey_CS_Example
         {
             try
             {
-                string baseUrl = "http://127.0.0.1:8000/api/check-license";
                 string ownerId = ""; // Set the owner ID
                 string secret = ""; // Set the secret
                 string name = ""; // Set the name
 
                 Api api = new Api(ownerId, secret, name);
-                string responseContent = await api.PostData(baseUrl, license_tb.Text.ToString());
+                string responseContent = await api.PostData(license_tb.Text.ToString());
 
                 MessageBox.Show(responseContent);
             }
